f3ba1a3304f11ccd002575be9309002d5af7af56
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-08-04 12:44:17 -0600

    added states

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -5,6 +5,13 @@
 #include <libTimer.h>
 #include "lcdutils.h"
 #include "lcddraw.h"
+typedef enum{
+  stateInitial,stateOne,stateTwo,
+}State;
+  int state1=0;
+  int state2=0;
+
+  State currentState=stateInitial;
 
 /** Initializes everything, clears the screen, draws "hello" and a square */
 int
@@ -19,5 +26,20 @@ main()
   drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
 
   fillRectangle(30,30, 60, 60, COLOR_ORANGE);
-  
+
+  //sudocode for my project
+  /*
+    if(state==state1){
+      button press x
+      state==state2
+    }
+    if(state==state2){
+      button pressy 
+      state==state3
+    }
+    if(state==state3){
+      button press z
+      state==state2
+    }
+  */
 }

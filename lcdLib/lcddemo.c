fd830b7e1732200b9c748c43b33216c547b9650a
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-08-05 20:01:01 -0600

    test 2 update

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -27,7 +27,7 @@ main()
 
   fillRectangle(30,30, 60, 60, COLOR_ORANGE);
   drawPixel(0,0,COLOR_WHITE); //bit (0,0) is in the top left
-  drawPixel(130,115,COLOR_WHITE); //upper range is 150,750
+  drawPixel(130,110,COLOR_WHITE); //upper range is 150,750
 
 
 

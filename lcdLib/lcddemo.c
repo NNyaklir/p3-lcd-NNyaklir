8eea1a7063ce3e77fb8d4403acc8cbe700f93d6d
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-08-06 13:34:26 -0600

    added button and states

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -5,13 +5,15 @@
 #include <libTimer.h>
 #include "lcdutils.h"
 #include "lcddraw.h"
+
+#define SW1 BIT0
+
 typedef enum{
   stateInitial,stateOne,stateTwo,
 }State;
-  int state1=0;
-  int state2=0;
 
   State currentState=stateInitial;
+
 
 /** Initializes everything, clears the screen, draws "hello" and a square */
 int
@@ -27,9 +29,49 @@ main()
 
   fillRectangle(30,30, 60, 60, COLOR_ORANGE);
   drawPixel(0,0,COLOR_WHITE); //bit (0,0) is in the top left
-  drawPixel(129,159,COLOR_WHITE); //upper range is 150,750
+  drawPixel(129,159,COLOR_WHITE); //upper range is 129,159
 
+  while(1)
+  {
+    int prevB1State = 1; // Previous state of button 1
 
+    while(1)
+    {
+      //read current button press
+      int b1State = (P2IN & SW1) ? 1 : 0;
+
+      //check for button press
+      if(b1State==0 && prevB1State==1)
+      {
+
+        if(currentState==stateInitial)
+
+        {
+          currentState = stateOne;
+          clearScreen(COLOR_PURPLE);
+
+        }
+
+        if(currentState==stateOne)
+        {
+          currentState =stateTwo;
+          clearScreen(COLOR_GREEN);
+        }
+
+        if(currentState==stateTwo)
+        {
+          clearScreen(COLOR_PURPLE);
+
+        }
+      }
+
+      prevB1State=b1State;
+
+      // Add a delay to avoid button bounce issues
+      __delay_cycles(200000);
+
+    }
+  }
 
 
   //sudocode for my project

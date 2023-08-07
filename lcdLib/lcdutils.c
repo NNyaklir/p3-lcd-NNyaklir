939ba11d8ed9bb7b93e7966463058e3b26b28b49
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-08-07 08:16:00 -0600

    Update lcdutils.c

--- lcdLib/lcdutils.c
+++ lcdLib/lcdutils.c
@@ -160,8 +160,8 @@ void lcd_init()
   }
 }
 
-#pragma vector=PORT2_VECTOR;
-interrupt void PORT2_ISR()
+#pragma vector=PORT2_VECTOR
+interrupt; void PORT2_ISR()
 {
     stopSongFlag = 1; // Set flag to stop the song
     P2IFG &= ~BIT0;  // Clear interrupt flag

8dd01c52cb4f83594a3a7d8b7e68d10b5bb1c928
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-08-07 08:50:45 -0600

    Update switch.s

--- lcdLib/switch.s
+++ lcdLib/switch.s
@@ -5,22 +5,22 @@ JT:
     .global main
 
 main2:
-    mov &correct,r12
-    cmp #6,r12
-    jge default
-    add r12,r12 
-    mov JT(r12),r0
+    MOV &correct,r12
+    CMP #6,r12
+    JGE default
+    ADD r12,r12 
+    MOV JT(r12),r0
 
 case1:
-    call #state1
-    jmp esc
+    CALL #state1
+    JMP esc
 
 case2:
-    call #state2
-    jmp esc
+    CALL #state2
+    JMP esc
 
 default:
-    jmp esc
+    JMP esc
 
 esc:
-    pop r0
+    POP r0

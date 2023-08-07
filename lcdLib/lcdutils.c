f074dee1f2720c9fa18fd78fdb3dedd3804b7a45
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-08-07 08:09:23 -0600

    Revert "previous commit working, this is trying to add interupt"
    This reverts commit 5c06be937ea08a8c6bdb48350e7133c3956c36e6.

--- lcdLib/lcdutils.c
+++ lcdLib/lcdutils.c
@@ -159,9 +159,3 @@ void lcd_init()
   }
 }
 
-#pragma vector=PORT2_VECTOR
-interrupt void PORT2_ISR(void)
-{
-    stopSongFlag = 1; // Set flag to stop the song
-    P2IFG &= ~BIT0;  // Clear interrupt flag
-}

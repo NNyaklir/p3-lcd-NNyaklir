ca046d46beeed58d4d8b0b86dce7a1b8a7ea2e57
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-08-07 08:43:58 -0600

    added assembly

---
+++ lcdLib/switch.s
@@ -0,0 +1,26 @@
+JT:
+    .word default
+    .word case1
+    .word case1
+    .global main
+
+main:
+    mov &correct,r12
+    cmp #6,r12
+    jge default
+    add r12,r12 
+    mov JT(r12),r0
+
+case1:
+    call #state1
+    jmp esc
+
+case2:
+    call #state2
+    jmp esc
+
+default:
+    jmp esc
+
+esc:
+    pop r0
